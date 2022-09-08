56ab98d157b3b628e75e55f5169953bb94af2120
mingze01.sun <mingze01.sun@singularity-ai.com>
2022-09-08 15:24:05 +0800

    update http request package

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/confluentinc/confluent-kafka-go v1.8.2
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-redis/redis/v8 v8.11.5
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/go-stack/stack v1.8.0
 	github.com/gomodule/redigo v1.8.8
 	github.com/json-iterator/go v1.1.12
@@ -70,6 +71,7 @@ require (
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
---
+++ simple_http_client/xRequest2/http_request.go
@@ -0,0 +1,83 @@
+package xRequest
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"git.singularity-ai.com/backend/library/utils"
+	"git.singularity-ai.com/backend/library/xContext"
+	"github.com/go-resty/resty/v2"
+	"time"
+)
+
+type HttpBase struct {
+	Url string
+	Cli *resty.Client
+}
+
+func (m *HttpBase) Post(ctx *xContext.XContext, req interface{}, resp interface{}) (err error) {
+	ctx.LogFields("request", m.Url)
+	ctx.SetTag(string(xContext.ReqBody), utils.MustJson(req))
+	ctx.SetTag("do_request", ctx.SerializeSpanContext())
+	ctx.SetTag("url", m.Url)
+	httpResp, err := m.Cli.R().
+		SetBody(req).
+		SetHeader("Content-Type", "application/json").
+		SetHeader("trace_id", ctx.SerializeSpanContext()).
+		SetHeader("time", time.Now().String()).
+		Post(m.Url)
+	if err != nil {
+		return err
+	}
+	ctx.SetTag("http_status", httpResp.StatusCode)
+	if httpResp.StatusCode() != 200 {
+		return errors.New(fmt.Sprintf("exception http code %v", httpResp.StatusCode()))
+	}
+	respBody := httpResp.Body()
+	ctx.SetTag(string(xContext.RespBody), string(respBody))
+	ctx.Debugf("resp=%v", string(respBody))
+	err = json.Unmarshal(respBody, resp)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//func (m *HttpBase) Get(ctx *xContext.XContext, req interface{}, resp interface{}) (err error) {
+//	bodyBytes, err := json.Marshal(req)
+//	if err != nil {
+//		return err
+//	}
+//	body := bytes.NewReader(bodyBytes)
+//	request, err := http.NewRequest(http.MethodGet, m.Url, body)
+//	if err != nil {
+//		return err
+//	}
+//	ctx.SetTag(string(xContext.ReqBody), string(bodyBytes))
+//	request.Header.Add("contentType", "application/json")
+//	ctx.SetTag("do_request", ctx.SerializeSpanContext())
+//	request.Header.Add("trace_id", ctx.SerializeSpanContext())
+//	httpResp, err := m.Cli.Do(request)
+//	if err != nil {
+//		return err
+//	}
+//	ctx.LogFields("request", m.Url)
+//	ctx.SetTag("url", m.Url)
+//	defer httpResp.Body.Close()
+//	ctx.SetTag("http_status", httpResp.StatusCode)
+//	if httpResp.StatusCode != 200 {
+//		return errors.New("except err != 200")
+//	}
+//	respBody, err := ioutil.ReadAll(httpResp.Body)
+//	if err != nil {
+//		ctx.SetTag(string(xContext.Error), "read resp body err")
+//		return errors.New("read resp body err")
+//	}
+//	ctx.SetTag(string(xContext.RespBody), string(respBody))
+//	ctx.Debugf("resp=%v", string(respBody))
+//	err = json.Unmarshal(respBody, resp)
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
