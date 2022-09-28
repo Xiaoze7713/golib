337190431ed50eced19d515232d7e9a8c625f7e3
mingze01.sun <mingze01.sun@singularity-ai.com>
2022-09-28 21:55:56 +0800

    add error

--- sync_instance/sync_instance.go
+++ sync_instance/sync_instance.go
@@ -1,6 +1,7 @@
 package sync_instance
 
 import (
+	"git.singularity-ai.com/backend/library/xContext/loggers/xlog"
 	"sync"
 	"time"
 )
@@ -18,19 +19,19 @@ type SyncManager[T SyncHandler, C ConfigAny] struct {
 	lock     sync.RWMutex
 	handler  *T
 	interval time.Duration
-	newF     func(config *C) *T
+	newF     func(config *C) (*T, error)
 	config   *C
 }
 
-func NewSyncMgr[T SyncHandler, C ConfigAny](newT func(config *C) *T, config *C, interval time.Duration) *SyncManager[T, C] {
-	t := newT(config)
+func NewSyncMgr[T SyncHandler, C ConfigAny](newT func(config *C) (*T, error), config *C, interval time.Duration) (*SyncManager[T, C], error) {
+	t, err := newT(config)
 	return &SyncManager[T, C]{
 		lock:     sync.RWMutex{},
 		handler:  t,
 		interval: interval,
 		newF:     newT,
 		config:   config,
-	}
+	}, err
 }
 
 func (m *SyncManager[T, C]) LoopUpdate() {
@@ -41,7 +42,11 @@ func (m *SyncManager[T, C]) LoopUpdate() {
 }
 
 func (m *SyncManager[T, C]) update() {
-	h := m.newF(m.config)
+	h, err := m.newF(m.config)
+	if err != nil {
+		xlog.Error(err)
+		return
+	}
 	m.lock.Lock()
 	defer m.lock.Unlock()
 	m.handler = h
