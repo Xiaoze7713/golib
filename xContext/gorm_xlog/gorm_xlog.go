f9703e2959a54e8bc6692ed636d5b4a915d30b41
mingze01.sun <sunmingze@singularity-ai.com>
2023-03-24 11:58:55 +0800

    udpate

--- xContext/gorm_xlog/gorm_xlog.go
+++ xContext/gorm_xlog/gorm_xlog.go
@@ -21,7 +21,6 @@ type GormXLog struct {
 	Ctx *xContext.XContext
 	//xLogger *xlog.Logger
 	level logger.LogLevel
-	trace bool
 }
 
 func NewGormXLog(ctx *xContext.XContext) (loggerIF logger.Interface) {
@@ -31,10 +30,6 @@ func NewGormXLog(ctx *xContext.XContext) (loggerIF logger.Interface) {
 func (m *GormXLog) LogMode(level logger.LogLevel) logger.Interface {
 	m.level = level
 	return m
-}
-
-func (m *GormXLog) SetTrace(trace bool) {
-	m.trace = trace
 }
 
 func (m *GormXLog) Info(ctx context.Context, s string, args ...interface{}) {
@@ -71,7 +66,7 @@ func (m *GormXLog) Trace(ctx context.Context, begin time.Time, fc func() (sql st
 	if m.level >= TraceLevel {
 		sql, rows := fc()
 		if xCtx, ok := ctx.(*xContext.XContext); ok {
-			xCtx.Debugf("[%s][T:%s][%s]ROWS %d, RES %v", m.Ctx.OperationName(), begin.Format("2006-01-02 15:04:05.999"), sql, rows, err)
+			xCtx.Debugf("[%s][T:%s][%s]ROWS %d, RES %v", xCtx.OperationName(), begin.Format("2006-01-02 15:04:05.999"), sql, rows, err)
 		} else {
 			m.Ctx.Debugf("[%s][T:%s][%s]ROWS %d, RES %v", m.Ctx.OperationName(), begin.Format("2006-01-02 15:04:05.999"), sql, rows, err)
 		}
